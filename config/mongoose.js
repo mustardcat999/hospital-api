e52305b5efcdb75d4e9033a704ba7e986e247cad
mustardcat999 <129050326+mustardcat999@users.noreply.github.com>
2023-07-25 07:11:25 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/hospital");
+mongoose.connect("mongodb+srv://hashBoy:b3HCsaCZprmZWGVi@cluster0.xz0ilri.mongodb.net/hospital?retryWrites=true&w=majority");
 const db = mongoose.connection;
 
 db.on("error", console.error.bind(console, "error connection to mongo"));
